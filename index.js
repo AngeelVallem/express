4e182ed9e34ff8d160ad34a6eaea29b2d412102e
Angel Valle <angelvalle@angel.local>
2021-05-12 20:45:39 -0500

    delete and get by id

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const newKoder = {
     "name" : "angel", 
     "gende" : "m"
 }
-
 //middleware
 app.use(express.json())  
-
 // Get koders json
     app.get('/koders', (req,res)=>{
-
         fs.readFile('koders.json', 'utf8',(err, data)=> {
             if(err){
                 console.log(err)
@@ -22,10 +19,8 @@ app.use(express.json())
             }
         })
     })
-
     // Add new koder
 app.post('/koders', (req,res)=> {
-    
     fs.readFile('koders.json', 'utf8',(err, data)=> {
         let json = JSON.parse(data)
         if(err){
@@ -44,17 +39,56 @@ app.post('/koders', (req,res)=> {
   
     res.end()
 })
-
 app.put('/koders', (req,res)=>{
     res.write('Aqui puedes sustituir un koder')
     res.end()
+}) 
+
+app.patch('/koders/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const name = req.body.name
+    
+    const content = fs.readFileSync('koders.json', 'utf8')
+    const json = JSON.parse(content) 
+    
+    const updateKoder  = json.koders.reduce((acc, cur) =>  {
+        if(id == cur.id){
+            cur.name = name
+        }
+        return [...acc, cur]
+    },[])
+    json.koder = updateKoder;
+    fs.writeFileSync('koders.json', JSON.stringify(json , null, 2), 'utf8')
+        res.json({"succes" : "true"})
+})
+
+app.delete('/koders/:id', (req,res)=> {
+
+    const content = fs.readFileSync('koders.json', 'utf8')
+    const json = JSON.parse(content)
+    const id = req.params.id
+    
+         json.koders.splice(id - 1, 1)
+    
+
+    fs.writeFileSync('koders.json', JSON.stringify( json , null, 2), 'utf8')
+    res.json({"succes" : "true"})
+})
+
+app.get('/koders/:id', (req,res)=> {
+    const id = req.params.id
+    const content = fs.readFileSync('koders.json', 'utf8')
+    const json = JSON.parse(content)
+
+        const koder = json.koders.reduce((acc,cur)=>  {
+            if(id == cur.id){
+                return res.json(cur)
+            }else{
+                console.log('No se encontro ningun koder con ese id');
+            }
+        } ,{})
 })
 
 app.listen(port,() => {
     console.log('server listening');
 })
-
-
-
-
-/**/ 
